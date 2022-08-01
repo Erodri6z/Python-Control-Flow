2920bbe22407c6ca082e6fd6a238f7d21493f209
Erodri6z <erikrodriguez2417@gmail.com>
2022-07-31 20:47:57 -0500

    complete rask 1

---
+++ exercise-1.py
@@ -0,0 +1,27 @@
+# exercise-01 Vowel or Consonant
+
+# Write the code that:
+# 1. Prompts the user to enter a letter in the alphabet:
+#      Please enter a letter from the alphabet (a-z or A-Z): 
+# 2. Write the code that determines whether the letter entered is a vowel
+# 3. Print one of following messages (substituting the letter for x):
+#      - The letter x is a vowel
+#      - The letter x is a consonant
+
+# Hints:  Use the in operator to check if a character is in another string
+#         For example, if some_char in 'abc':
+
+letter = input('Enter a letter from A-Z/a-z:')
+
+if (letter.lower()) == 'a':
+  print('That is a Vowel')
+elif (letter.lower()) == 'e':
+  print('That is a vowel')
+elif (letter.lower()) == 'i':
+  print('That is a vowel')
+elif (letter.lower()) == 'o':
+  print('That is a vowel')
+elif (letter.lower()) == 'u':
+  print('That is a vowel')
+else:
+  print('pretty sure thats a constant')
